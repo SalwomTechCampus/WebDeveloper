0792fcdb8f4f16d9272847d27b1385ee1bbb825f
salwom <salwom@live.com>
2019-04-12 15:53:27 +0300

    جلسه طويلة عن العمل

--- scripts.js
+++ scripts.js
@@ -1,43 +1,41 @@
 
-	 	function getRandomColor(){
-	 		var letter='0123456789ABCDEF'.split('');
-	 		var color='#';
-	 		for (var i=0;i<6;i++){
-	 			color+=letter[Math.round(Math.random()*15)];
-	 		}
-	 		return color;
-	 	}
-	 	
-		var clickTime; var createdTime; var reaction;
-	function boxcalled(){
-		var x = Math.random();
-			x=x*1000;
-		setTimeout(function(){
-			if(Math.random()>.5){
-				document.getElementById("circle").style.borderRadius="100px";}
-			else{
-				document.getElementById("circle").style.borderRadius="0px";
+		function getRandomColor() {
+			var letter = '0123456789ABCDEF'.split('');
+			var color = '#';
+			for (var i = 0; i < 6; i++) {
+				color += letter[Math.round(Math.random() * 15)];
 			}
+			return color;
+		}
 
-			document.getElementById("circle").style.backgroundColor=getRandomColor();
-				document.getElementById("circle").style.display="block";
-			createdTime=Date.now();
-		}, x);
+		var clickTime; var createdTime; var reaction;
+		function boxcalled() {
+			var x = Math.random();
+			x = x * 1000;
+			setTimeout(function () {
+				if (Math.random() > .5) {
+					document.getElementById("circle").style.borderRadius = "100px";
+				}
+				else {
+					document.getElementById("circle").style.borderRadius = "0px";
+				}
 
-	}
-	document.getElementById("circle").onclick=function(){
-			clickTime=Date.now();
+				document.getElementById("circle").style.backgroundColor = getRandomColor();
+				document.getElementById("circle").style.display = "block";
+				createdTime = Date.now();
+			}, x);
 
-			reaction=(clickTime-createdTime)/1000;
-			document.getElementById("dateTime").innerHTML=(clickTime-createdTime)/1000;
+		}
+		document.getElementById("circle").onclick = function () {
+			clickTime = Date.now();
 
-		this.style.display="none";
+			reaction = (clickTime - createdTime) / 1000;
+			document.getElementById("dateTime").innerHTML = (clickTime - createdTime) / 1000;
+
+			this.style.display = "none";
 
 
-		boxcalled();
+			boxcalled();
 		}
 		boxcalled();
-		
-		
 
-	
